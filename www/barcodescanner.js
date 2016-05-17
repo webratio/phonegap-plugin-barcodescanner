webratio/phonegap-plugin-barcodescanner

commit 5780924cc5d15efac8be7db3728dfb6c0121edac

Updated plugin for webratio customization

www/barcodescanner.js
@@ -101,7 +101,7 @@
         };
 
         //-------------------------------------------------------------------
-        BarcodeScanner.prototype.encode = function (type, data, successCallback, errorCallback, options) {
+        BarcodeScanner.prototype.encode = function (type, data, format, successCallback, errorCallback, options) {
             if (errorCallback == null) {
                 errorCallback = function () {
                 };
@@ -118,7 +118,7 @@
             }
 
             exec(successCallback, errorCallback, 'BarcodeScanner', 'encode', [
-                {"type": type, "data": data, "options": options}
+                {"type": type, "data": data, "format": format, "options": options}
             ]);
         };
 

.gitignore
@@ -1,4 +1,3 @@
 .DS_Store
 bin/
 .classpath
-/build.xml

plugin.xml
@@ -1,14 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?><plugin xmlns="http://www.phonegap.com/ns/plugins/1.0"
     xmlns:android="http://schemas.android.com/apk/res/android"
-    id="com.phonegap.plugins.barcodescanner"
-    version="2.2.0">
+    id="com.webratio.phonegap.plugins.barcodescanner"
+    version="1.2.9">
 
     <name>BarcodeScanner</name>
     <description>You can use the BarcodeScanner plugin to scan different types of barcodes (using the device's camera) and get the metadata encoded in them for processing within your application.</description>
     <license>MIT</license>
 
-    <repo>https://github.com/wildabeast/BarcodeScanner</repo>
-    <issue>https://github.com/wildabeast/BarcodeScanner/issues</issue>
+    <repo>https://github.com/webratio/phonegap-plugin-barcodescanner</repo>
 
     <engines>
         <engine name="cordova" version=">=3.0.0" />
@@ -68,14 +67,13 @@
         <config-file target="AndroidManifest.xml" parent="/manifest/application">
             <activity
                 android:name="com.google.zxing.client.android.CaptureActivity"
-                android:screenOrientation="landscape"
                 android:clearTaskOnLaunch="true"
                 android:configChanges="orientation|keyboardHidden"
                 android:theme="@android:style/Theme.NoTitleBar.Fullscreen"
                 android:windowSoftInputMode="stateAlwaysHidden"
                 android:exported="false">
                 <intent-filter>
-                    <action android:name="com.google.zxing.client.android.SCAN"/>
+                    <action android:name="com.phonegap.plugins.barcodescanner.SCAN"/>
                     <category android:name="android.intent.category.DEFAULT"/>
                 </intent-filter>
             </activity>
@@ -111,6 +109,7 @@
         <source-file src="src/android/LibraryProject/res/drawable/launcher_icon.png" target-dir="res/drawable"/>
         <source-file src="src/android/LibraryProject/res/drawable/share_via_barcode.png" target-dir="res/drawable"/>
         <source-file src="src/android/LibraryProject/res/drawable/shopper_icon.png" target-dir="res/drawable"/>
+        <source-file src="src/android/LibraryProject/res/drawable/flip_camera.png" target-dir="res/drawable"/>
         <source-file src="src/android/LibraryProject/res/drawable-hdpi/launcher_icon.png" target-dir="res/drawable-hdpi"/>
         <source-file src="src/android/LibraryProject/res/drawable-hdpi/shopper_icon.png" target-dir="res/drawable-hdpi"/>
         <source-file src="src/android/LibraryProject/res/drawable-xhdpi/launcher_icon.png" target-dir="res/drawable-xhdpi"/>
@@ -314,7 +313,7 @@
         <framework src="src/windows/lib/WinRTBarcodeReader.csproj" custom="true" type="projectReference"/>
     </platform>
 
-    <!-- Windows Phone 8 -->
+    <!-- wp8 -->
     <platform name="wp8">
         <config-file target="config.xml" parent="/*">
             <feature name="BarcodeScanner">
@@ -326,14 +325,11 @@
             <Capability Name="ID_CAP_ISV_CAMERA" />
         </config-file>
 
-        <framework src="src/wp8/lib/zxing.wp8.0.dll" custom="true" />
-
-        <asset src="src/wp8/assets/cancel.png" target="Images/appbar.cancel.png" />
-
         <source-file src="src/wp8/BarcodeScanner.cs" />
-        <source-file src="src/wp8/BarcodeScannerTask.cs" />
-        <source-file src="src/wp8/BarcodeScannerUI.xaml" />
-        <source-file src="src/wp8/BarcodeScannerUI.xaml.cs" />
+        <source-file src="src/wp8/BarcodeScannerResult.cs" />
+        <source-file src="src/wp8/Scan.xaml" />
+        <source-file src="src/wp8/Scan.xaml.cs" />
+        <framework src="src/wp8/zxing.wp8.0.dll" custom="true" />
     </platform>
 
     <!-- browser -->

src/android/LibraryProject/AndroidManifest.xml
@@ -51,7 +51,6 @@
   <application android:icon="@drawable/launcher_icon"
                android:label="@string/app_name">
     <activity android:name=".CaptureActivity"
-              android:screenOrientation="landscape"
               android:clearTaskOnLaunch="true"
               android:stateNotNeeded="true"
               android:configChanges="orientation|keyboardHidden"

src/android/LibraryProject/res/layout-ldpi/capture.xml
@@ -216,4 +216,11 @@
             android:text="@string/msg_default_status"
             android:textColor="@color/status_text"/>
 
+  <Button android:id="@+id/flip_button"
+          android:layout_width="wrap_content"
+          android:layout_height="wrap_content"
+          android:layout_gravity="top|left"
+          android:drawableLeft="@drawable/flip_camera"
+          android:visibility="invisible"/>
+
 </FrameLayout>

src/android/LibraryProject/res/layout/capture.xml
@@ -215,4 +215,11 @@
             android:text="@string/msg_default_status"
             android:textColor="@color/status_text"/>
 
+  <Button android:id="@+id/flip_button"
+          android:layout_width="wrap_content"
+          android:layout_height="wrap_content"
+          android:layout_gravity="top|left"
+          android:drawableLeft="@drawable/flip_camera"
+          android:visibility="invisible"/>
+
 </FrameLayout>

src/android/LibraryProject/src/com/google/zxing/PlanarYUVLuminanceSource.java
@@ -45,7 +45,7 @@ public final class PlanarYUVLuminanceSource extends LuminanceSource {
     super(width, height);
 
     if (left + width > dataWidth || top + height > dataHeight) {
-      throw new IllegalArgumentException("Crop rectangle does not fit within image data.");
+//      throw new IllegalArgumentException("Crop rectangle does not fit within image data. Left:" + left + ", width:" + width + ", dataWidth:" + dataWidth + ", top:" + top + ", height:" + height + ", dataHeight:" + dataHeight);
     }
 
     this.yuvData = yuvData;

commit d3550b4e2d5e202143671e8f3a9f17330eeb964b

Removed breaking API change
The new "format" parameter introduced by
5780924cc5d15efac8be7db3728dfb6c0121edac was not a compatible extension
to the original API of the base plugin. We are now using an option
instead.

www/barcodescanner.js
@@ -101,7 +101,7 @@
         };
 
         //-------------------------------------------------------------------
-        BarcodeScanner.prototype.encode = function (type, data, format, successCallback, errorCallback, options) {
+        BarcodeScanner.prototype.encode = function (type, data, successCallback, errorCallback, options) {
             if (errorCallback == null) {
                 errorCallback = function () {
                 };
@@ -118,7 +118,7 @@
             }
 
             exec(successCallback, errorCallback, 'BarcodeScanner', 'encode', [
-                {"type": type, "data": data, "format": format, "options": options}
+                {"type": type, "data": data, "format": (options && options.format), "options": options}
             ]);
         };
 